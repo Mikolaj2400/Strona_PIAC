85746f7fd7af12a2c826a7c7ff372793f7b35dee
Mikolaj2400 <mikka2000@interia.pl>
2022-04-07 15:56:09 +0200

    "lab 03"

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask import render_template
 from flask import make_response, abort
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
+mail = Mail(app)
+
+app.config('MAIL_SERVER') = 'smtp.gmail.com'
+app.config('MAIL_PORT') =   465
+app.config('MAIL_USERNAME') = 'yourId@gmail.com'
+app.config('MAIL_PASSWORD') = '*****'
+app.config('MAIL_USE_TLS') = False
+app.config('MAIL_USE_SSL') = True
+mail = Mail(app)
 
 @app.route('/error_denied')
 def error_denied():
@@ -23,7 +34,14 @@ def not_found_error(error):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html'), "sent"
+    msg = Message("Hello", sender = 'yourId@gmail.com', recipients = ['someone1@gmail.com'])
+    msg.body = "Hello Flask message sent from Flask-Mail"
+    mail.send(msg)
+
+@app.route('s')
+def szablon():
+    return render_template('index.html', user="Stan", email="dowolny@email.pl")
 
 @app.route("/index")
 def homePage():
