25427dab5cf845797db5bbc8262eefa8b67f49fc
Mikolaj2400 <mikka2000@interia.pl>
2022-05-25 23:00:41 +0200

    Adding bootstrap

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import make_response, abort
 #from flask import Mail
-from AzureDB import AzureDB
+#from AzureDB import AzureDB
 
 app = Flask(__name__)
 
